221029e62dee131922dbc6cfa9e4f188bb66561f
2kauacosta-dot <2kauacosta@gmail.com>
2026-06-15 21:40:03 -0300

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,1 +1,23 @@
 
+const config = require('../config.json');
+
+module.exports = {
+    name: 'messageCreate',
+    execute(message, client) {
+        if (message.author.bot) return;
+        if (!message.content.startsWith(config.prefix)) return;
+
+        const args = message.content.slice(config.prefix.length).trim().split(/ +/);
+        const commandName = args.shift().toLowerCase();
+
+        const command = client.commands.get(commandName);
+        if (!command) return;
+
+        try {
+            command.execute(message, args, client);
+        } catch (error) {
+            console.error(error);
+            message.reply('❌ Erro ao executar o comando.');
+        }
+    }
+};
