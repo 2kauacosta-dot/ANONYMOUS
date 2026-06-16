a71c8ace3a0e7ee028876c71ad371dad6949f272
2kauacosta-dot <2kauacosta@gmail.com>
2026-06-15 21:39:11 -0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,1 +1,17 @@
 
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    name: 'avatar',
+    execute(message, args) {
+        const usuario = message.mentions.users.first() || message.author;
+
+        const embed = new EmbedBuilder()
+            .setTitle(`🖼️ Avatar de ${usuario.username}`)
+            .setImage(usuario.displayAvatarURL({ dynamic: true, size: 1024 }))
+            .setColor('Blue')
+            .setFooter({ text: `Solicitado por ${message.author.username}` });
+
+        message.reply({ embeds: [embed] });
+    }
+};
